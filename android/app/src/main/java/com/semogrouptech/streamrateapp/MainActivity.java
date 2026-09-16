3438603ae35b6de1cfede15bb2183945fcf9cd0a
Kagiso Sekete <126578628+Kagisosekete1@users.noreply.github.com>
2026-09-16 21:10:25 +0200

    Fix Android branding, system bars, and Firebase config

---
+++ android/app/src/main/java/com/semogrouptech/streamrateapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.semogrouptech.streamrateapp;
+
+import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+  }
+}
